bd40d019102c3ba9f3b6c3d3ba8ee9b9d7061a8e
zhaoyongtao <285929232@qq.com>
2018-07-14 11:57:54 +0800

    定位后关闭

--- app/src/main/java/com/shen/house/SplashActivity.kt
+++ app/src/main/java/com/shen/house/SplashActivity.kt
@@ -6,12 +6,18 @@ import android.os.Bundle
 import android.view.View
 import com.shen.baselibrary.base.BaseActivity
 import com.shen.baselibrary.utiles.FileUtils
+import com.shen.baselibrary.utiles.ToastUtile
 import com.shen.baselibrary.utiles.resulttutils.PermissionCallBack
 import com.shen.baselibrary.utiles.resulttutils.PermissionUtils
+import com.zaaach.citypicker.CityPicker
+import com.zaaach.citypicker.LocationUtils
+import com.zaaach.citypicker.adapter.OnPickListener
+import com.zaaach.citypicker.model.CityBean
+import com.zaaach.citypicker.model.LocateState
 import java.io.File
 
 class SplashActivity : BaseActivity() {
-    override fun getcontentView() = -1
+    override fun getcontentView() = R.layout.activity_splash
 
     override fun injectStatus(view: View) = null
 
@@ -36,13 +42,50 @@ class SplashActivity : BaseActivity() {
     }
 
     private fun toNextActivity() {
-        if (ConfigUtils.getCurrentCity()!=null) {
+//        if (ConfigUtils.getCurrentCity() != null) {
+        if (true) {
             startActivity(Intent(`this`, MainActivity::class.java))
-        }else{
-            startActivities(arrayOf(Intent(`this`, MainActivity::class.java),Intent(`this`, CityActivity::class.java)))
+            overridePendingTransition(0, 0);
+            finish()
+        } else {
+//            startActivities(arrayOf(Intent(`this`, MainActivity::class.java),Intent(`this`, CityActivity::class.java)))
+            CityPicker.getInstance()
+                    .setFragmentManager(supportFragmentManager)
+                    .enableAnimation(true)
+                    .setLocatedCity(ConfigUtils.getCurrentCity())
+                    .setOnPickListener(object : OnPickListener {
+                        override fun onPick(position: Int, data: CityBean?) {
+                            if (data != null) {
+                                data.pinyin = "当前城市"
+                                ConfigUtils.setCurrentCity(data)
+                            } else {
+                                ConfigUtils.setCurrentCity(CityBean("北京", "当前城市", ""))
+                            }
+                            startActivity(Intent(`this`, MainActivity::class.java))
+                            overridePendingTransition(0, 0);
+                            finish()
+                        }
+
+                        override fun onLocate() {
+                            //开始定位，这里模拟一下定位
+                            getLocation()
+                        }
+                    })
+                    .show()
         }
 
-        overridePendingTransition(0, 0);
-        finish()
+
+    }
+
+    private fun getLocation() {
+        LocationUtils.locationCity(`this`) { city ->
+            if (city != null) {
+                ToastUtile.showToast("定位成功")
+                CityPicker.getInstance().locateComplete(city, LocateState.SUCCESS)
+                ConfigUtils.setCurrentCity(city)
+            } else {
+                CityPicker.getInstance().locateComplete(CityBean(getString(com.zaaach.citypicker.R.string.cp_locate_failed), "当前城市", "0"), LocateState.FAILURE)
+            }
+        }
     }
 }
