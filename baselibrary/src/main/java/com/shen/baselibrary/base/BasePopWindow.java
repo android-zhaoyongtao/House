a1d531d25af2706bc9d0423104ff34aa448fdf3b
zhaoyongtao <285929232@qq.com>
2018-07-17 00:35:03 +0800

    Revert "opupshow位置不对"
    This reverts commit ff9dd6c

--- baselibrary/src/main/java/com/shen/baselibrary/base/BasePopWindow.java
+++ baselibrary/src/main/java/com/shen/baselibrary/base/BasePopWindow.java
@@ -2,16 +2,12 @@ package com.shen.baselibrary.base;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.PopupWindow;
 
-import com.shen.baselibrary.ContextHouse;
 import com.shen.baselibrary.utiles.KeyBoardUtils;
-import com.shen.baselibrary.utiles.LogUtils;
 import com.shen.baselibrary.utiles.ViewUtils;
 
 
@@ -46,23 +42,7 @@ public abstract class BasePopWindow extends PopupWindow {
             makeContentViewWidthExactly(width);
             setWidth(width);
             setHeight(LayoutParams.WRAP_CONTENT);
-
-            Rect r = new Rect();
-            parent.getGlobalVisibleRect(r);
-            int bottom = r.bottom;
-            LogUtils.e("zhaoy", "bottom:" + bottom);
-            int marginBottom = ContextHouse.SCREENHEIGHT - bottom;//parent到屏幕底部距离
-
-            getContentView().measure(0, 0);
-            int contentHeight = getContentView().getMeasuredHeight();
-            if (marginBottom < contentHeight) {//如果占不下这个框了
-                super.showAtLocation(parent, Gravity.TOP | Gravity.START, r.left, ContextHouse.SCREENHEIGHT - contentHeight);
-            } else {
-                this.showAsDropDown(parent);
-            }
-
-
-
+            this.showAsDropDown(parent);
             ViewUtils.backgroundAlpha((Activity) parent.getContext(), 0.5f);
         } else {
             this.dismiss();
