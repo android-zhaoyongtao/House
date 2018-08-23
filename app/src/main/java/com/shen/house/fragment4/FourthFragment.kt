a35acd4ef4d8c801f202f4891a9949ef9566ec62
尼古拉斯 <285929232@qq.com>
2018-08-23 21:22:58 +0800

    LiveDataBus

--- app/src/main/java/com/shen/house/fragment4/FourthFragment.kt
+++ app/src/main/java/com/shen/house/fragment4/FourthFragment.kt
@@ -1,14 +1,34 @@
 package com.shen.house.fragment4
 
+import android.arch.lifecycle.Observer
 import android.view.View
+import android.widget.TextView
 import com.shen.baselibrary.base.BaseFragment
+import com.shen.baselibrary.utiles.LiveDataBus
 import com.shen.house.R
+
 
 class FourthFragment : BaseFragment() {
     override fun getcontentView() = R.layout.fragment_fourth
 
     override fun afterInjectView(view: View) {
+        val button = view.findViewById<TextView>(R.id.button)
+        val textView = view.findViewById<TextView>(R.id.textView)
+        LiveDataBus.get().with("key_test").setValue(1)
+        LiveDataBus.get().with("key_test").setValue(2)
+        LiveDataBus.get().with("key_test").setValue(3)
+        button.setOnClickListener {
+            LiveDataBus.get().with("key_test").setValue(666)
+        }
+        LiveDataBus.get().with("key_test", Int::class.java)
+                .observe(this, object : Observer<Int> {
+                    override fun onChanged(aBoolean: Int?) {
+//                        ToastUtile.showToast("" + aBoolean)
+//                        textView.setText("" + aBoolean)
+                        aBoolean?.let { textView.append(" " + aBoolean) }
 
+                    }
+                })
     }
 
     override fun getData() {
@@ -16,3 +36,4 @@ class FourthFragment : BaseFragment() {
     }
 
 }
+
